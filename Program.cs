da45679a1d8f08dcebd516342bdaddb030e70188
corneliajohnson <corneliaj91@gmail.com>
2020-11-13 11:26:58 -0500

    added random answers

--- Program.cs
+++ Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MagicMoose
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      // Let the moose speak!
+      MooseSays("I'm the Magic Moose. Ask Me Anything!");
+      string question = Console.ReadLine();
+      MooseSays(RandomAnswer());
     }
+
+    static void MooseSays(string message)
+    {
+      Console.WriteLine($@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+            ");
+    }
+
+    static string RandomAnswer()
+    {
+      List<string> answers = new List<string>()
+        {
+            "As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.","Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", "Yes – definitely.", "You may rely on it."
+        };
+      Random randomResponse = new Random();
+      int generateRandom = randomResponse.Next(0, answers.Count - 1);
+      return answers[generateRandom];
+    }
+  }
 }
